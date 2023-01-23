d477b3f1086b478a05c21916e9218882ee538f46
Matt Jakubowski <jakubowski@gmail.com>
2023-01-22 18:07:41 -0600

    pathing

--- server/src/endpoints/deeplink.js
+++ server/src/endpoints/deeplink.js
@@ -1,7 +1,7 @@
 
 import express from 'express'
-import * as Util from './../utils/helper.js';
-import * as DB from './../utils/db.js';
+import * as Util from '../utils/helper.js';
+import * as DB from '../utils/db.js';
 
 var router = express.Router();
 
