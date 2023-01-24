f00ac2f1450cb7954c77daea705f0b6497a1c351
Matt Jakubowski <jakubowski@gmail.com>
2023-01-24 12:20:53 -0600

    path issue

--- server/src/endpoints/category/lenses.js
+++ server/src/endpoints/category/lenses.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import * as Util from '../utils/helper.js';
-import * as DB from '../utils/db.js';
+import * as Util from '../../utils/helper.js';
+import * as DB from '../../utils/db.js';
 
 import fs from 'fs';
 
